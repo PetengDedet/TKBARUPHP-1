0e7ffba2394813939252d168e2f5c090f573dcb3
Peteng Dedet <pueteng.dedet@gmail.com>
2016-09-30 21:10:33 +0700

    Update index.blade.php

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -24,10 +24,39 @@
             <h3 class="box-title">@lang('customer.index.title')</h3>
         </div>
         <div class="box-body">
-            {!! $dataTable->table() !!}
+            <table class="table table-condensed table-hover table-stripped">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Customer</th>
+                        <th>Address</th>
+                        <th>No. Telp</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+            
+                @forelse($customer as $k => $v)
+                    <tr>
+                        <td>{{$k+1}}</td>
+                        <td>{{$v->name}}</td>
+                        <td>{{$v->address}}</td>
+                        <td>{{$v->phone}}</td>
+                        <td>
+                             <div class="btn-group btn-group-xs">
+                                <a href="/dashboard/master/customer/show/{{$v->id}}" class="btn  btn-default" style="color:blue;"><i class="fa fa-eye"></i></a>
+                                <a href="/dashboard/master/customer/edit/{{$v->id}}" class="btn btn-default" style="color:green;"><i class="fa fa-pencil"></i></a>
+                                <a href="/dashboard/master/customer/delete/{{$v->id}}" class="btn btn-default" onclick="return confirm(\'Hapus?\')" style="color:red;"><i class="fa fa-trash"></i></a>
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                @endforelse
+            </table>
+            {{-- {!! $dataTable->table() !!} --}}
         </div>
         <div class="box-footer clearfix">
             <a class="btn btn-success" href="{{ route('db.master.customer.create') }}"><span class="fa fa-plus fa-fw"></span>&nbsp;@lang('customer.index.button.create')</a>
+            {{$customer->links()}}
         </div>
     </div>
 
@@ -37,7 +66,7 @@
     <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('js/dataTables.bootstrap.min.js')}}"></script>
     <script src="/vendor/datatables/buttons.server-side.js"></script>
-    {!! $dataTable->scripts() !!}
+    {{-- {!! $dataTable->scripts() !!} --}}
     <script type="text/javascript">
         $(document).ready(function () {
 //            notie.alert(1, 'Success!', 0.5);
